986f3afad7ef39354f2beadba19f8dba8c0e32aa
Cristina Gámez <163881246+cristinagher@users.noreply.github.com>
2024-05-21 18:56:28 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,10 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
-import '../scss/styles.scss';
+import "../scss/styles.scss";
+//Crea una función que reciba un número, la función debe imprimir una cuenta atrás desde ese número hasta 0.
+
+const firstBucle = (number) => {
+  for (let counter = number; counter >= 0; counter--) {
+    console.log(counter);
+  }
+};
+firstBucle(4);
